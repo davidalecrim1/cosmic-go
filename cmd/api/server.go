a2d138eb297eed67feb828b9628f7d2fe550e1a6
David Alecrim <dsalecrim@outlook.com>
2024-10-03 17:58:51 -0300

    chore: improve server start

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -5,16 +5,22 @@ import (
 	"cosmic-go/internal/handler"
 	"cosmic-go/internal/infra/repository"
 	"cosmic-go/internal/service"
+	"log"
 	"net/http"
 )
 
 func main() {
 	db := bootstrap.InitializeDatabase()
+	defer db.Close()
 
 	repo := repository.NewPostgresRepository(db)
 	svc := service.NewService(repo)
 	handler := handler.NewHandler(svc)
 
 	router := bootstrap.InitializeRouter(handler)
-	http.ListenAndServe(":8080", router)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalln("Server failed to start:", err)
+	}
 }
