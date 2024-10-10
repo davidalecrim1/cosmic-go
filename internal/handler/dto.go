f26a93352ffb6add2d6fdc74197f2aa83be980dc
David Alecrim <dsalecrim@outlook.com>
2024-10-10 10:10:12 -0300

    feat: dto in its file

---
+++ internal/handler/dto.go
@@ -0,0 +1,33 @@
+package handler
+
+import "time"
+
+type AllocationRequest struct {
+	OrderID  string `json:"orderid" validate:"required"`
+	SKU      string `json:"sku" validate:"required"`
+	Quantity int    `json:"quantity" validate:"required"`
+}
+
+type DeallocateRequest struct {
+	OrderID string `json:"orderid" validate:"required"`
+	SKU     string `json:"sku" validate:"required"`
+}
+
+type AddBatchRequest struct {
+	Reference         string     `json:"reference" validate:"required"`
+	Product           ProductDTO `json:"product"`
+	PurchasedQuantity int        `json:"purchased_quantity" validate:"required,gt=0"`
+	ETA               time.Time  `json:"eta"`
+}
+
+type ProductDTO struct {
+	SKU string `json:"sku" validate:"required"`
+}
+
+type AllocationResponse struct {
+	BatchRef string `json:"batchref"`
+}
+
+type BadRequestResponse struct {
+	Message string `json:"message"`
+}
